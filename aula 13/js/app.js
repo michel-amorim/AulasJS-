5419236e10b24afd6c0f263d447de42a893b6e37
michel-amorim <michelmsa7799@gmail.com>
2021-11-04 00:06:29 -0300

    feat: merge - aula - 13 - strings

---
+++ aula 13/js/app.js
@@ -0,0 +1,12 @@
+let umaString = "Um texto";
+
+console.log(umaString);
+
+// concat - concatenar
+// indexOf ->
+// lastIndexOf <-
+// match -- expressão regular
+// Replace -- repetição
+// length -- descobre a quantidade de caracteres
+// slice ou substring -- ??
+// toUpperCase -- Deixa a strig tudo maiusculo -- toLowwercase -- minusulo
