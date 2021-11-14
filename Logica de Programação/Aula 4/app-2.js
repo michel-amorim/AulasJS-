fcb16d0c6b05ad5afe76f415a9ca43f972c7a9f4
Michel Amorim <michelmsa7799@gmail.com>
2021-11-14 03:02:14 -0300

    if, else e else if

---
+++ Logica de Programação/Aula 4/app-2.js
@@ -0,0 +1,24 @@
+// Exercicios if, else, else if
+
+/*
+Se(if) (numero >= 0 && numero <= 5) faça isso {código}
+Se não(else) faça isso {código}
+*/
+
+const numero = 10;
+
+if (numero <= 10) {
+  console.log("O numero é menor ou igual a 10.");
+}
+
+if (numero >= 0 && numero <= 5) {
+  console.log("O número está entre 0 e 5.");
+} else if (numero >= 6 && numero <= 8) {
+  console.log("O número NÃO está entre 6 e 8.");
+} else if (numero >= 9 && numero <= 11) {
+  console.log("O número está entre 9 e 11.");
+} else {
+  console.log("O número NÃO está entre 0 e 11.");
+}
+
+console.log("...Aqui vai o restante do código");
