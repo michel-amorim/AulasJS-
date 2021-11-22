9ad71c096f72d7b0c014af5eca318001b4002bf3
Michel Amorim <michelmsa7799@gmail.com>
2021-11-22 11:13:39 -0300

    Operação ternaria

---
+++ Logica de Programação/Aula 6/main.js
@@ -0,0 +1,18 @@
+// Operação Ternaria
+
+// (condição) ? 'valor para verdadeiro' : 'Valor para falso'
+
+const pontuaçãoUsuario = 999;
+const nivelUsuario =
+  pontuaçãoUsuario >= 1000 ? "Usuario VIP" : "Usuário normal";
+
+/*
+O exemplo acima faz exatamente a mesma coisa que o exemplo a baixo
+
+if (pontuaçãoUsuario >= 1000) {
+  console.log("Usuário VIP");
+} else {
+  console.log("Usuário Normal");
+}
+
+*/
