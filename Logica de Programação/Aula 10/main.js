3e835b7fded780eb88acce3bc17981b9ac7fb763
Michel Amorim <michelmsa7799@gmail.com>
2021-11-25 08:11:29 -0300

    finalizando aula var e let

--- Logica de Programação/Aula 10/main.js
+++ Logica de Programação/Aula 10/main.js
@@ -1,20 +1,41 @@
-// DIferençça entre Let e Var
+// Diferençça entre Let e Var
 
-let nome = "Luiz";
+// let nome = "Luiz";
 // let nome = "Otavio";
 
 // não pode redeclarar let no mesmo escopo: typeErro: Identifier 'nome' has already been declared
 
 var nome2 = "Luiz";
-var nome2 = "Luiz";
+var nome2 = "Michel"; // redeclarando VAR
 
-// Var pode sobrescrever normalmente
+// Var pode ser redeclarada dentro do mesmo escopo normalmente
 
 const verdadeira = true;
+
 if (verdadeira) {
-  let nome = "Otavio";
+  let nome = "Silva";
   console.log(nome, nome2);
+
+  if (verdadeira) {
+    let nome = "Julian";
+    console.log(nome, nome2);
+  }
 }
 
 // Let -- tem escopo de bloco { ... isso é um bloco ... }
 // Var -- Só tem escopo de função
+
+function falaOi() {
+  var nome3 = "Pedro";
+  if (verdadeira) {
+    let nome4 = "Amorim";
+    console.log(nome4);
+  }
+}
+
+falaOi();
+
+//
+
+console.log(sobrenome);
+var sobrenome = "Miranda";
