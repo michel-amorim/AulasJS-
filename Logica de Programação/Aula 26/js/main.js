1ac91391efa16c2fb05999aa23eb8d181dea4fa8
Michel Amorim <michelmsa7799@gmail.com>
2021-12-05 20:26:11 -0300

    relogio

---
+++ Logica de Programação/Aula 26/js/main.js
@@ -0,0 +1,34 @@
+function criaHoradosSegundos(segundos) {
+  const data = new Date(segundos * 1000);
+  return data.toLocaleDateString("pt-BR", {
+    hour12: false,
+    timeZone: "UTC",
+  });
+}
+
+const relogio = document.querySelector(".relogio");
+const iniciar = document.querySelector(".iniciar");
+const pausar = document.querySelector(".pausar");
+const zerar = document.querySelector(".zerar");
+let segundos = 0;
+let timer;
+
+function iniciaRelogio() {
+  timer = setInterval(function () {
+    segundos++;
+    relogio.innerHTML = criaHoraDosSegundos(segundos);
+  }, 1000);
+}
+
+iniciar.addEventListener("click", function (event) {
+  iniciaRelogio();
+});
+
+pausar.addEventListener("click", function (event) {
+  clearInterval(timer);
+});
+
+zerar.addEventListener("click", function (event) {
+  clearInterval(timer);
+  relogio.innerHTML = "00:00:00";
+});
