cfd4ef85db836d1f6ff6d417ee49e6f591ac8a9e
Michel Amorim <michelmsa7799@gmail.com>
2021-12-05 21:07:13 -0300

    relogio

--- Logica de Programação/Aula 26/js/main.js
+++ Logica de Programação/Aula 26/js/main.js
@@ -1,34 +1,41 @@
-function criaHoradosSegundos(segundos) {
-  const data = new Date(segundos * 1000);
-  return data.toLocaleDateString("pt-BR", {
-    hour12: false,
-    timeZone: "UTC",
+function relogio() {
+  function criaHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString("pt-BR", {
+      hour12: false,
+      timeZone: "UTC",
+    });
+  }
+
+  let segundos = 0;
+  let timer;
+
+  function iniciaRelogio() {
+    timer = setInterval(function () {
+      segundos++;
+      relogio.innerHTML = criaHoraDosSegundos(segundos);
+    }, 1000);
+  }
+
+  document.addEventListener("click", function (e) {
+    const el = e.target;
+
+    if (el.classList.contains("zerar")) {
+      clearInterval(timer);
+      relogio.classList.add("zerarBlue");
+      relogio.innerHTML = "00:00:00";
+      segundos = 0;
+    }
+    if (el.classList.contains("iniciar")) {
+      relogio.classList.remove("pausado");
+      relogio.classList.remove("zerarBlue");
+      clearInterval(timer);
+      iniciaRelogio();
+    }
+    if (el.classList.contains("pausar")) {
+      relogio.classList.add("pausado");
+      clearInterval(timer);
+    }
   });
 }
-
-const relogio = document.querySelector(".relogio");
-const iniciar = document.querySelector(".iniciar");
-const pausar = document.querySelector(".pausar");
-const zerar = document.querySelector(".zerar");
-let segundos = 0;
-let timer;
-
-function iniciaRelogio() {
-  timer = setInterval(function () {
-    segundos++;
-    relogio.innerHTML = criaHoraDosSegundos(segundos);
-  }, 1000);
-}
-
-iniciar.addEventListener("click", function (event) {
-  iniciaRelogio();
-});
-
-pausar.addEventListener("click", function (event) {
-  clearInterval(timer);
-});
-
-zerar.addEventListener("click", function (event) {
-  clearInterval(timer);
-  relogio.innerHTML = "00:00:00";
-});
+relogio();
