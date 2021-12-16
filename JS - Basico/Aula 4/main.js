4cdb36ec3eb50aaf16da42f298069c62e7956c8d
Michel Amorim <michelmsa7799@gmail.com>
2021-12-16 00:34:00 -0300

    variavel com LET

---
+++ JS - Basico/Aula 4/main.js
@@ -0,0 +1,24 @@
+// variavel com LET
+
+let nomeM = "Michel";
+let nomeF = "Juliana";
+
+console.log(nomeM, "nasceu em 1984");
+console.log("Em 2000", nomeM, "conheceu", nomeF);
+console.log(nomeM, "casou-se com", nomeF, "em 2012");
+
+//
+
+nomeM = "Eduardo";
+nomeF = "Fernanda";
+
+// pode-se mudar o valor da variavel quando quiser
+
+console.log(nomeF, "teve 1 filho com", nomeM, "em 2015");
+console.log("O filho de", nomeM, "com", nomeF, "chama-se Eduardo");
+
+// não pode criar variavéis com palavras reservadas
+// não pode criar variavéis com numeros
+// não pode criar variavéis com espaços ou traços
+// utiliza camelCase
+// case-sensitive = maiusculo e minusculo importam
