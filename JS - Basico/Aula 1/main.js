479024318ca3fd9a9aeac52e23a06c1b3a9f3e37
Michel Amorim <michelmsa7799@gmail.com>
2021-12-15 22:51:55 -0300

    console.log

---
+++ JS - Basico/Aula 1/main.js
@@ -0,0 +1,11 @@
+// Metado log dentro do objeto console
+
+// serve para exibir algo na tela
+
+console.log("Michel Silva"); // utilizando valor literal
+
+// `` -- template strings
+
+// String = texto
+
+console.log(123456); // number não utiliza aspas ou crase
