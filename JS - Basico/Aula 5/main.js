c6a1242c2c6174164d2d5f0c78488f2c2b5e8d4b
Michel Amorim <michelmsa7799@gmail.com>
2021-12-16 02:01:44 -0300

    const

---
+++ JS - Basico/Aula 5/main.js
@@ -0,0 +1,13 @@
+// constate com const
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const conta = primeiroNumero * segundoNumero;
+
+console.log(typeof primeiroNumero); // verificando se é um Number ou String
+
+// não pode criar const com palavras reservadas
+// não pode criar const com numeros
+// não pode criar const com espaços ou traços
+// utiliza camelCase
+// case-sensitive = maiusculo e minusculo importam
