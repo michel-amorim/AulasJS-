de616d81fa25be57c7619e89366340cf8fb87777
Michel Amorim <michelmsa7799@gmail.com>
2021-12-16 23:17:30 -0300

    Dados primitivos

---
+++ JS - Basico/Aula 8/main.js
@@ -0,0 +1,18 @@
+// Dados primitivos
+
+// String, number, undefined, null, boolean, symbol (veremos no futuro)
+
+const nome1 = "luiz"; // String --> texto
+const nome2 = "luiz"; // String
+const nome3 = "luiz"; // String
+
+const num1 = 10; // Number --> numero
+const num2 = 20; // Number
+
+let nomeAluno; // Undefined = Não aponta pra local nenhum
+
+/* Não se pode inicializar um const sem valor, por isso mudei para let */
+
+const sobrenomeAluno = null; // Não aponta pra local nenhum
+
+const aprovado = true; // Boolean --> true, false
