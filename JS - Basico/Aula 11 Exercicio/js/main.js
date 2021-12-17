40f5ebe09151395e9e68a64c22cd0feb1554d8b4
Michel Amorim <michelmsa7799@gmail.com>
2021-12-17 02:39:05 -0300

    exercicio concluido

---
+++ JS - Basico/Aula 11 Exercicio/js/main.js
@@ -0,0 +1,12 @@
+// Exercicio com Prompt, alert
+
+let num1 = prompt("Digite um numero");
+let num2 = prompt("Digite um numero");
+
+num1 = Number(num1);
+num2 = Number(num1);
+
+alert(`Seu resultado foi ${num1 + num2}`);
+/*
+Quando se usa TemplateStrings você pode executar uma expressão dentro da templeta
+*/
