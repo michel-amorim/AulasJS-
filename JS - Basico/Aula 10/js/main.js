dd53264b327b3281829a70722d1ee4b00cb59d34
Michel Amorim <michelmsa7799@gmail.com>
2021-12-17 02:29:06 -0300

    Alert, confirm e prompt

---
+++ JS - Basico/Aula 10/js/main.js
@@ -0,0 +1,6 @@
+// Elemento central do navegador 'objeto window'
+// ctrl + l -- limpar o console do navegador
+
+window.confirm("Deseja realmente apagar"); // tela window para pergntar para usuario
+
+window.prompt("Digite seu nome"); // caixa para digitar texto
