cba88f1c94f18644e1cd0a36f88d34f2db01ade4
Michel Amorim <michelmsa7799@gmail.com>
2022-01-15 23:31:00 -0300

    funcao

---
+++ JS - Funções/Aula 8/main.js
@@ -0,0 +1,42 @@
+// Funções fábricas (Factory Functions -> this)
+
+function criaPessoa(nome, sobrenome, a, p) {
+  return {
+    nome,
+    sobrenome,
+
+    // Getter
+
+    get nomeCompleto() {
+      return `${this.nome} ${this.sobrenome}`;
+    },
+
+    // Setter
+
+    set nomeCompleto(valor) {
+      valor = valor.split(" ");
+      this.nome = valor.shift();
+      this.sobrenome = valor.join(" ");
+    },
+
+    fala(assunto = "falando sobre NADA") {
+      return `${this.nome} tem ${assunto}`;
+    },
+
+    altura: a,
+    peso: p,
+
+    get imc() {
+      const indice = this.peso / this.altura ** 2;
+      return indice.toFixed(2);
+    },
+  };
+}
+const p1 = criaPessoa("Luiz", "Otávio", 1.8, 80);
+const p2 = criaPessoa("Michel", "Silva", 1.9, 100);
+p1.nomeCompleto = "Michel Silva Amorim";
+
+console.log(p1.nome);
+console.log(p1.sobrenome);
+console.log(p1.fala());
+//
