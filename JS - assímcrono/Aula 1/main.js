939bea1130c9821751494be631c8102b914fa252
Michel Amorim <michelmsa7799@gmail.com>
2022-01-17 23:25:49 -0300

    Promises

---
+++ JS - assímcrono/Aula 1/main.js
@@ -0,0 +1,40 @@
+function rand(min, max) {
+  min *= 1000;
+  max *= 1000;
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function esperaAi(msg, temp) {
+  return new Promise((resolve, reject) => {
+    if (typeof msg !== "String") reject(new Error("ERRO"));
+
+    setTimeout(() => {
+      resolve(msg);
+    }, temp);
+  });
+}
+
+esperaAi("Conexão com o base de dados", rand(1, 3))
+  .then((resposta) => {
+    console.log(resposta);
+    return esperaAi("Buscando dados da BASE", rand(1, 3))
+      .then((resposta) => {
+        console.log(resposta);
+        return esperaAi(22222, rand(1, 3));
+      })
+      .then((resposta) => {
+        console.log(resposta);
+      })
+      .then(() => {
+        console.log("Exibe dados na tela");
+      });
+  })
+  .catch((e) => {
+    console.log("Erro", e);
+  });
+
+console.log("Isso aqui será exibido antes de qualquer promisse");
+
+// resolve -> Esse codigo execcutou com sucesso então resolve ele para mim
+
+// reject -> Deu um erro no codigo, rejeita isso para mim
