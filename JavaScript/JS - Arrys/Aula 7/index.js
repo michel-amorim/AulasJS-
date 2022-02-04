47e63601f842393d774490dd7d81d27172dfbedc
Michel Amorim <michelmsa7799@gmail.com>
2022-02-04 12:17:59 -0300

    unificando tudo map, reduce, filter

---
+++ JavaScript/JS - Arrys/Aula 7/index.js
@@ -0,0 +1,13 @@
+// Filter + map + Reduce
+
+// Filtrar numeros pares
+// Dobrar os Valores
+// Reduzir (Somar tudo)
+
+const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 27];
+
+const numerosPares = numeros
+  .filter((valor) => valor % 2 === 0)
+  .map((valor) => valor * 2)
+  .reduce((acumulador, valor) => acumulador + valor);
+console.log(numerosPares);
