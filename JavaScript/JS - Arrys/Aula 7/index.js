b41a4fc2f7bfb6f6c4d6005b3b880529ff7f2f6a
Michel Amorim <michelmsa7799@gmail.com>
2022-02-04 12:18:34 -0300

    refatorando

--- JavaScript/JS - Arrys/Aula 7/index.js
+++ JavaScript/JS - Arrys/Aula 7/index.js
@@ -9,5 +9,5 @@ const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 27];
 const numerosPares = numeros
   .filter((valor) => valor % 2 === 0)
   .map((valor) => valor * 2)
-  .reduce((acumulador, valor) => acumulador + valor);
+  .reduce((ac, valor) => ac + valor);
 console.log(numerosPares);
