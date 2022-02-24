7b12a33c7b0f4c48894ede84cdcfd70c42d9c754
Michel Amorim <michelmsa7799@gmail.com>
2022-02-23 21:36:11 -0300

    metodos uteis para objetos

--- JavaScript/JS - Objetos e prototypes/Aula 4/index.js
+++ JavaScript/JS - Objetos e prototypes/Aula 4/index.js
@@ -3,14 +3,28 @@
 const produto = { nome: "caneca", preco: 1.8 };
 const caneca = Object.assign({}, produto, { material: "porcelana" });
 
-Object.assign(); // serve para copiar um objeto
-
-caneca.nome = "Camisa";
-console.log(produto);
-console.log(caneca);
+// Object.assign(); // serve para copiar um objeto
 
 // freeze
 
-Object.freeze(); // voce nao podera mais adicionar, deleter... dentro do seu objeto
+//Object.freeze(); // voce nao podera mais adicionar, deleter... dentro do seu objeto
+
+// keys
 
 console.log(Object.keys(produto)); // voce vê as chaves dentro do seu objeto
+
+// getOwnPropertyDescriptor
+
+console.log(Object.getOwnPropertyDescriptor(produto, "nome"));
+// mostra o que é essa propiedade
+
+// values
+
+console.log(Object.values(produto)); // mostra apenas os valores
+
+// entries
+
+for (let valor of Object.entries(produto)) {
+  console.log(valor);
+}
+// retorna chave e valores porem em array
